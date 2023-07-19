3fa5824079a40b9240b71d8312d071dd912aa8e3
mechane <medyokr@gmail.com>
2023-07-19 19:18:14 +0100

    minimap

--- src/rendering/mini_map.c
+++ src/rendering/mini_map.c
@@ -6,7 +6,7 @@
 /*   By: mechane <mechane@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/16 13:26:35 by mechane           #+#    #+#             */
-/*   Updated: 2023/07/19 17:46:59 by mechane          ###   ########.fr       */
+/*   Updated: 2023/07/19 19:17:52 by mechane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,7 @@ static void	ft_draw(int x, int y, t_data *mlx, int color)
 				int dx = i1 - 100;
 				int dy = j1 - 100;
 				int distance = sqrt(dx * dx + dy * dy);
-				if (distance <= 98)
+				if (distance <= 96)
 					mlx_put_pixel(mlx->img, j1, i1, color);
 			}
 			j++;
@@ -49,43 +49,17 @@ static void	ft_draw(int x, int y, t_data *mlx, int color)
 }
 void draw_circle(t_data *mlx,int center_x, int center_y, int radius)
 {
-    for (int y = 0; y < 250; ++y) {
-        for (int x = 0; x < 250; ++x)
+    for (int y = 0; y < 200; ++y) {
+        for (int x = 0; x < 200; ++x)
 		{
             int dx = x - center_x;
             int dy = y - center_y;
             int distance = sqrt(dx * dx + dy * dy);
 
-            if (distance < radius)
-				mlx_put_pixel(mlx->img,x, y, 0);
-			else if (distance >= radius && distance <= radius + 2)
-				mlx_put_pixel(mlx->img,x, y, (190 << 24| 63 << 8 |120));	
+            if (distance <= radius)
+				mlx_put_pixel(mlx->img,x, y, 0);	
         }
     }
-}
-void	draw_line_p(t_point *p2, t_point *p1, t_data *mlx)
-{
-	double	steps;
-	t_point	d;
-	t_point	p;
-	t_point	inc;
-
-	d.y = p2->y - p1->y;
-	d.x = p2->x - p1->x;
-	if (fabs(d.x) > fabs(d.y))
-		steps = fabs(d.x);
-	else
-		steps = fabs(d.y);
-	inc.x = d.x / steps;
-	inc.y = d.y / steps;
-	p.x = p1->x;
-	p.y = p1->y;
-	while (steps-- >= 0)
-	{
-		mlx_put_pixel(mlx->img, p.x, p.y,0);
-		p.x += inc.x;
-		p.y += inc.y;
-	}
 }
  void	ft_draw_p(t_data *mlx, int color)
 {
@@ -94,11 +68,11 @@ void	draw_line_p(t_point *p2, t_point *p1, t_data *mlx)
 	double	ang;
 
 	ang = to_rad(mlx->plr->rotate_ang);
-	end_x = 100 + 15 * cos(ang);
-	end_y = 100 + 15 * sin(ang);
+	end_y = 100.0 + 3.0 * cos(ang);
+	end_x = 100.0 + 3.0 * sin(ang);
 	(void)color;
-	draw_line_p(&(t_point){100,100}, &(t_point){end_x, end_y}, mlx);
-	draw_circle(mlx, 100, 100, 1);
+	draw_circle(mlx, 100, 100, 5);
+	mlx_put_pixel(mlx->img, end_x, end_y,(255 << 24 | 255 << 16|255<< 8|255));
 }
 
 void	render_mini_map(t_map *m, t_data *mlx)
@@ -108,7 +82,7 @@ void	render_mini_map(t_map *m, t_data *mlx)
 	int		y;
 	
 	map = m->map;
-	draw_circle(mlx,100,100,100);
+	draw_circle(mlx,100,100, 96);
 	x = 0;
 	while (map[(x)])
 	{
@@ -123,7 +97,6 @@ void	render_mini_map(t_map *m, t_data *mlx)
 		}
 		x++;
 	}
-	// draw_player(mlx, 100 , 100, mlx->plr->rotate_ang, 10);
 	ft_draw_p(mlx, (255 << 24 | 255));
 }
 
